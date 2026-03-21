2eefe9ebdb1a9cefbe22a85bcbae540d0124ea39
Manoj Kumar <f20231103@goa.bits-pilani.ac.in>
2026-03-21 02:19:14 -0400

    Point frontend to Render backend

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: '/',
+  baseURL: 'https://performance-dashboard-api.onrender.com',
 });
 
 API.interceptors.request.use((config) => {
